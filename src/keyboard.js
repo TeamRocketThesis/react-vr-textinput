9ef62ed45d006e18ac4aa576449919c4e56904f2
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-06-06 11:35:01 -0700

    added boilterplate files

---
+++ src/keyboard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, VrButton, StyleSheet, Text } from 'react-vr';
+
+class Keyboard extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+export default Keyboard;
