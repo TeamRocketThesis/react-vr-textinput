78227cd4bc3f8c0159c7713490cec39c9701a5a0
Rishi Raje <hrishiraje@gmail.com>
2017-06-06 14:07:18 -0700

    Add keyboard render method

--- src/keyboard.js
+++ src/keyboard.js
@@ -23,11 +23,34 @@ getLayout () {
   }
 }
 
+
+
   render() {
     var layoutArray = this.getLayout();
+    numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     return (
       <View>
-
+      <View>
+        {numberArray.map((number) => <KeyboardButton value={number} clickHandler={this.handleAllValues.bind(this)} isDisabled={false}/> )}
+        <KeyboardButton value={'Delete'} clickHandler={this.handleDelete.bind(this)} isDisabled={false} />
+      </View>
+      <View>
+        {layoutArray[0].map((value) => <KeyboardButton value={value} clickHandler={this.handleAllValues.bind(this)} isDisabled={false}/> )}
+      </View>
+      <View>
+        {layoutArray[1].map((value) => <KeyboardButton value={value} clickHandler={this.handleAllValues.bind(this)} isDisabled={false}/> )}
+      </View>
+      <View>
+        <KeyboardButton value={'Shift'} clickHandler={this.handleShift.bind(this)} isDisabled={this.state.isShiftSelected}/>
+        {layoutArray[2].map((value) => <KeyboardButton value={value} clickHandler={this.handleAllValues.bind(this)} isDisabled={false}/> )}
+        <KeyboardButton value={this.state.isSymbolSelected ? layout.alphabet.displayValue : layout.symbol.displayValue} clickHandler={this.symbolSelector.bind(this)} isDisabled={false} />
+      </View>
+      <View>
+        <KeyboardButton value={'Back'} clickHandler={this.handleBack.bind(this)} isDisabled={false} />
+        <KeyboardButton value={'Forward'} clickHandler={this.handleForward.bind(this)} isDisabled={false} />
+        <KeyboardButton value={''} clickHandler={this.handleSpacebar.bind(this)} isDisabled={false} />
+        <KeyboardButton value={'Submit'} clickHandler={this.handleSubmit.bind(this)} isDisabled={false} />
+      </View>
       </View>
     );
   }
