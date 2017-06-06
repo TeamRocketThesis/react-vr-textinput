c74424adc3ae121e2d9cc009d904f1a31da1a04f
Simmba <simmern.kang@gmail.com>
2017-06-06 14:55:12 -0700

    working on styling continuued

--- examples/index.vr.js
+++ examples/index.vr.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppRegistry, asset, Pano, Text, View} from 'react-vr';
-import Keyboard from './src/keyboard';
+import Keyboard from '../src/keyboard';
 export default class Example extends React.Component {
   render() {
     return (
