6e47ffa6ce3863e6c7db3f0875eb30d1996f3d5a
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-06-06 12:12:47 -0700

    changed code back to development

--- vr/client.js
+++ vr/client.js
@@ -8,7 +8,7 @@ import {VRInstance} from 'react-vr-web';
 function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'Example', parent, {
     // Add custom options here
-    // enableHotReload: true,
+    enableHotReload: true,
     ...options,
   });
   vr.render = function() {
