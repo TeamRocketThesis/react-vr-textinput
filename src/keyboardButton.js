85fe6e374f96c8944aaf58f9e286a5a98f8e1d5a
Simmba <simmern.kang@gmail.com>
2017-06-06 14:55:12 -0700

    working on styling continuued

--- src/keyboardButton.js
+++ src/keyboardButton.js
@@ -3,22 +3,28 @@ import { View, VrButton, StyleSheet, Text } from 'react-vr';
 
 var styles = StyleSheet.create({
   text: {
-    color: 'white',
-    alignItems: 'center',
+    fontSize: 0.075,
     textAlign: 'center',
-    fontSize = 0.2,
+    color: '#f5f5f5',
+    opacity: 1
   },
   button: {
-    borderWidth: 0.5,
-    borderColor: '#A9A9A9',
+    padding: 0.05,
+    borderStyle: 'solid',
+    borderColor: 'white',
+    flex: 1,
+    flexDirection: 'column',
+    transform: [{translate: [0,0,-2]}],
+    backgroundColor: 'black',
+    opacity: .65,
   }
 })
 
 class KeyboardButton extends Component {
   render() {
     return (
-        <VrButton styles={styles.button}>
-          <Text style={styles.text}>{this.props.something}</Text>
+        <VrButton style={styles.button}>
+        <Text style={styles.text}>A</Text>
         </VrButton>  
     );
   }
