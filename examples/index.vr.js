34daf1b144b6fe677dd1d903ee53e4af3f8015f7
Rishi Raje <hrishiraje@gmail.com>
2017-06-07 12:27:31 -0700

    Fixed render method in textInput

--- examples/index.vr.js
+++ examples/index.vr.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { AppRegistry, asset, Pano, Text, View} from 'react-vr';
 import Keyboard from '../src/keyboard';
+import TextInput from '../src/textInput'
 
 export default class Example extends React.Component {
   render() {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
-        <Keyboard />
+        <TextInput/>
       </View>
     );
   }
--- src/textInput.js
+++ src/textInput.js
@@ -9,13 +9,13 @@ class TextInput extends Component {
     this.state = {
       cursorPosition: '0',
       textArrayCursorYes: ['*'],
-      textArrayCursorNo: [''],
+      textArrayCursorNo: [' '],
       text: '',
       selected: false,
       rows: this.props.rows || 4,
       columns: this.props.cols || 50,
       submitHandler: this.props.onSubmit || null,
-      showScroll
+      showScroll: false
     }
   }
 
@@ -37,7 +37,28 @@ handleAllLetters(value) {
 }
 
 handleDelete() {
-  this.setState()
+
+  this.setState({
+
+    textArrayCursorYes: function(){
+      var newArray = this.state.textArrayCursorYes.slice();
+      newArray.splice(this.state.cursorPosition -1, 1, '*');
+      newArray.splice(this.state.cursorPosition, 1);
+      return newArray;
+    }(),
+
+    textArrayCursorNo: function() {
+      var newArray = this.state.textArrayCursorNo.slice();
+      newArray.splice(this.state.cursorPosition -1, 1, '*');
+      newArray.splice(this.state.cursorPosition, 1);
+      return newArray;
+    }(),
+
+    cursorPosition: this.state.cursorPosition - 1,
+
+    text: this.generateText()
+
+  });
 
 }
 
@@ -61,33 +82,54 @@ handleDown() {
 
 }
 
-paginate() {
+paginate(s) {
+  if(s.length <= this.state.columns) return [s];
+  
+  var array = [];
+  var pointer1 = 0;
+  var pointer2 = this.state.columns;
+  var done = false;
+  
+  while(!done) {
+    // get string from pointer 1 to pointer 2 
+    var sub = s.slice(pointer1, pointer2);
+    //check if it contains * ... if yes if asterisk is at last position then pointer2++
+    if(sub.includes('*')) pointer2 ++;
+    //push pointer 1 - pointer2 string to array
+    array.push(s.slice(pointer1, pointer2));
+    //move pointer 1 to pointer2
+    pointer1 = pointer2;
+    //check if pointer1 + cols is less than s length. If yes then pointer2 = length and done is true. Else pointer2 = pointer1 + cols
+    if(pointer1 + this.state.columns < s.length) {
+      pointer2 = pointer1 + this.state.columns;
+    } else {
+      pointer2 = s.length;
+      done = true;
+    }
 
+  }
+      
+  if(s.slice(pointer1).length!== 0) array.push(s.slice(pointer1));
+  return array;
 }
 
   render() {
-var array = this.paginate();
-if(array.length > rows) {
-  this.setState({
-    showScroll: true
-  });
-}
+var arrayCursorYes = this.paginate(this.state.textArrayCursorYes);
+console.log('arrayCursorYes ', arrayCursorYes);
+var arrayCursorNo = this.paginate(this.state.textArrayCursorNo);
+console.log('arrayCursorNo ', arrayCursorNo);
+// if(array.length > rows) {
+//   this.setState({
+//     showScroll: true
+//   });
+// }
 return(<View>
-  <View> {/* this view might need to be a VR button to handle focus / defocus */}
-    <Text>{/* Need a way of showing the correct section of our paginated text*/}</Text>
+  <View>
+    <Text>{this.state.text}</Text>
     </View>
-
-    <Scroll handleUp={this.handleUp.bind(this)} 
-    handleDown={this.handleDown.bind(this)} />
-
-    <Keyboard handleSubmit={this.handleSubmit.bind(this)} 
-    handleAllLetters={this.handleAllLetters.bind(this)} 
-    handleDelete={this.handleDelete.bind(this)} 
-    handleForward={this.handleForward.bind(this)} 
-    handleBack={this.handleBack.bind(this)} />
-
+    <Scroll handleUp={this.handleUp.bind(this)} handleDown={this.handleDown.bind(this)} />
+    <Keyboard handleSubmit={this.handleSubmit.bind(this)} handleAllLetters={this.handleAllLetters.bind(this)} handleDelete={this.handleDelete.bind(this)} handleForward={this.handleForward.bind(this)} handleBack={this.handleBack.bind(this)} />
 </View>);
-
   }
 }
 
