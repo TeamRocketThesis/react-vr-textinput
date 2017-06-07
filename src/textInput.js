0dae5daed5d445382e630c5ebfab0f89e58e7dc3
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-06-07 16:26:03 -0700

    handle delete functionality

--- src/textInput.js
+++ src/textInput.js
@@ -7,57 +7,54 @@ class TextInput extends Component {
   constructor(props){
     super(props);
     this.state = {
-      cursorPosition: '0',
-      textArrayCursorYes: ['*'],
-      textArrayCursorNo: [' '],
+      cursorPosition: 0,
+      textArrayCursorYes: '|',
+      textArrayCursorNo: ' ',
       text: '',
       selected: false,
       rows: this.props.rows || 4,
       columns: this.props.cols || 50,
       submitHandler: this.props.onSubmit || null,
-      showScroll: false
+      showScroll: false,
+      toggleCursor: true
     }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        toggleCursor: !this.state.toggleCursor
+      })
+    }, 500)
   }
 
 generateText() {
   var string = '';
   for(var i =0; i < this.state.textArrayCursorYes.length; i++) {
-    if(this.state.textArrayCursorYes[i] != '*') string += this.state.textArrayCursorYes[i];
+    if(this.state.textArrayCursorYes[i] !== '|') string += this.state.textArrayCursorYes[i];
   }
   return string;
 }
 
 handleAllLetters(value) {
+  var newArrYes = this.state.textArrayCursorYes.slice(0, this.state.cursorPosition) + value + this.state.textArrayCursorYes.slice(this.state.cursorPosition);
+  var newArrNo = this.state.textArrayCursorNo.slice(0, this.state.cursorPosition) + value + this.state.textArrayCursorNo.slice(this.state.cursorPosition);
   this.setState({
-    textArrayCursorYes: this.state.textArrayCursorYes.splice(this.state.cursorPosition, 0, value),
-    textArrayCursorNo: this.state.textArrayCursorNo.splice(this.state.cursorPosition, 0, value),
+    textArrayCursorYes: newArrYes,
+    textArrayCursorNo: newArrNo,
     cursorPosition: this.state.cursorPosition + 1,
     text: this.generateText()
   });
 }
 
 handleDelete() {
-
+  var arr = this.state.textArrayCursorYes.slice(0, this.state.cursorPosition - 1) + this.state.textArrayCursorYes.slice(this.state.cursorPosition);
+  var arr2 = this.state.textArrayCursorNo.slice(0, this.state.cursorPosition - 1) + this.state.textArrayCursorNo.slice(this.state.cursorPosition);
+  
   this.setState({
-
-    textArrayCursorYes: function(){
-      var newArray = this.state.textArrayCursorYes.slice();
-      newArray.splice(this.state.cursorPosition -1, 1, '*');
-      newArray.splice(this.state.cursorPosition, 1);
-      return newArray;
-    }(),
-
-    textArrayCursorNo: function() {
-      var newArray = this.state.textArrayCursorNo.slice();
-      newArray.splice(this.state.cursorPosition -1, 1, '*');
-      newArray.splice(this.state.cursorPosition, 1);
-      return newArray;
-    }(),
-
-    cursorPosition: this.state.cursorPosition - 1,
-
-    text: this.generateText()
-
+    textArrayCursorYes: arr,
+    textArrayCursorNo: arr2,
+    cursorPosition: this.state.cursorPosition - 1
   });
 
 }
@@ -94,7 +91,7 @@ paginate(s) {
     // get string from pointer 1 to pointer 2 
     var sub = s.slice(pointer1, pointer2);
     //check if it contains * ... if yes if asterisk is at last position then pointer2++
-    if(sub.includes('*')) pointer2 ++;
+    if(sub.includes('|')) pointer2++;
     //push pointer 1 - pointer2 string to array
     array.push(s.slice(pointer1, pointer2));
     //move pointer 1 to pointer2
@@ -114,10 +111,10 @@ paginate(s) {
 }
 
   render() {
-var arrayCursorYes = this.paginate(this.state.textArrayCursorYes);
-console.log('arrayCursorYes ', arrayCursorYes);
-var arrayCursorNo = this.paginate(this.state.textArrayCursorNo);
-console.log('arrayCursorNo ', arrayCursorNo);
+    var arrayCursorYes = this.paginate(this.state.textArrayCursorYes);
+    // console.log('arrayCursorYes ', arrayCursorYes);
+    var arrayCursorNo = this.paginate(this.state.textArrayCursorNo);
+    // console.log('arrayCursorNo ', arrayCursorNo);
 // if(array.length > rows) {
 //   this.setState({
 //     showScroll: true
@@ -125,7 +122,9 @@ console.log('arrayCursorNo ', arrayCursorNo);
 // }
 return(<View>
   <View>
-    <Text>{this.state.text}</Text>
+    <View style={{ transform: [{ translate: [0, 0.2, -1.5] }] }}>
+      <Text>{this.state.toggleCursor ? this.state.textArrayCursorYes : this.state.textArrayCursorNo}</Text> 
+    </View>
     </View>
     <Scroll handleUp={this.handleUp.bind(this)} handleDown={this.handleDown.bind(this)} />
     <Keyboard handleSubmit={this.handleSubmit.bind(this)} handleAllLetters={this.handleAllLetters.bind(this)} handleDelete={this.handleDelete.bind(this)} handleForward={this.handleForward.bind(this)} handleBack={this.handleBack.bind(this)} />
