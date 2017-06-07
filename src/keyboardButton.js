e7281bf29f9c68a2f82fcc7350dba19ecdf94c0d
Simmba <simmern.kang@gmail.com>
2017-06-07 11:34:53 -0700

    idea in progress

--- src/keyboardButton.js
+++ src/keyboardButton.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, VrButton, StyleSheet, Text } from 'react-vr';
-
+import { View, VrButton, StyleSheet, Text, Animated } from 'react-vr';
+var AnimatedVrButton = Animated.createAnimatedComponent(VrButton);
 var styles = StyleSheet.create({
   text: {
     fontSize: 0.04,
@@ -17,26 +17,41 @@ var styles = StyleSheet.create({
     flex: 1,
     transform: [{translate: [-1,0,-1.5]}],
     alignItems: 'center',
-    opacity: 0.5,
   }
 })
 class KeyboardButton extends Component {
     constructor(props){
       super(props);
       this.state = {
-        backgroundColor: '#0d0d0d'
+        backgroundColor: '#0d0d0d',
+        opacity: 0.5,
+        fade: new Animated.Value(0.5),
+        clicked: false
       }
     }
+  fade() {
+    console.log("HEY");
+    this.setState({clicked: true},
+    Animated.timing(this.state.fade, {   // and twirl
+      toValue: 0,
+      duration: 1000
+    }),
+    Animated.timing(this.state.fade, {
+      toValue: 0.5,
+      duration: 100
+    }).start)
+    this.setState({clicked:false});
+  }
   render() {
     return (
-      <VrButton 
+      <AnimatedVrButton 
       onClick={this.props.isDisabled === false ? this.props.clickHandler.bind(this, this.props.value) : null}
-      style={[styles.button, {backgroundColor: this.props.isDisabled === false? this.state.backgroundColor: 'red'}]}
+      style={[styles.button, {backgroundColor: this.props.isDisabled === false? this.state.backgroundColor: 'red'}, {opacity: this.state.clicked === false? this.state.opacity:this.state.fade}]}
       onEnter={() => this.setState({backgroundColor: 'green'})}
       onExit={() => this.setState({backgroundColor: '#0d0d0d'})}
       >
-        <Text style={styles.text}>{this.props.value}</Text>
-      </VrButton>  
+        <Text style={styles.text} onClick={()=>this.fade()}>{this.props.value}</Text>
+      </AnimatedVrButton>  
     );
   }
 }
