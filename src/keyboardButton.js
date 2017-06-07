77b6b134351a961bdbdfed5fc0ebca442c07a7d5
Simmba <simmern.kang@gmail.com>
2017-06-07 10:49:38 -0700

    hovering effect

--- src/keyboardButton.js
+++ src/keyboardButton.js
@@ -16,16 +16,25 @@ var styles = StyleSheet.create({
     borderWidth: 0.005,
     flex: 1,
     transform: [{translate: [-1,0,-1.5]}],
-    backgroundColor: '#0d0d0d',
     alignItems: 'center',
     opacity: 0.5,
   }
 })
-
 class KeyboardButton extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        backgroundColor: '#0d0d0d'
+      }
+    }
   render() {
     return (
-      <VrButton onClick={this.props.isDisabled === false ? this.props.clickHandler.bind(this, this.props.value) : null} style={styles.button}>
+      <VrButton 
+      onClick={this.props.isDisabled === false ? this.props.clickHandler.bind(this, this.props.value) : null}
+      style={[styles.button, {backgroundColor: this.state.backgroundColor}]}
+      onEnter={() => this.setState({backgroundColor: 'red'})}
+      onExit={() => this.setState({backgroundColor: '#0d0d0d'})}
+      >
         <Text style={styles.text}>{this.props.value}</Text>
       </VrButton>  
     );
