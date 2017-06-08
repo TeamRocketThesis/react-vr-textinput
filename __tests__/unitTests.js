2a510063afc8d767636a406cdda3a7e461a676b9
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-06-08 16:05:19 -0700

    added test framework and cursor auto scroll on delete and enter letters

--- __tests__/unitTests.js
+++ __tests__/unitTests.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, asset, Pano, Text, View } from 'react-vr';
 import Keyboard from '../src/keyboard';
-import TextInput from '../src/textInput'
+import TextInput from '../src/textInput';
+import chai, {expect, should} from 'chai';
 
+
+describe('client', function () {
+
+  
+
+
+});
