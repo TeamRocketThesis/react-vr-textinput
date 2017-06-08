8a87720ac28348b2cc40d304e0f07592b17de48c
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-06-07 17:04:54 -0700

    added imports for test file

---
+++ __tests__/unitTests.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { AppRegistry, asset, Pano, Text, View } from 'react-vr';
+import Keyboard from '../src/keyboard';
+import TextInput from '../src/textInput'
+
